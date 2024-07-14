9bae62bf978e7df30b90a1411674eb464b5c1451
codin2 <codin2@hotmail.com>
2024-07-14 14:25:04 -0400

    Delete relay-control directory

--- relay-control/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Unistalling go-librespot-daemon"
-
-rm /usr/bin/go-librespot
-rm /lib/systemd/system/go-librespot-daemon.service
-
-echo "Done"
-echo "pluginuninstallend"
--- relay-control/utils/extendedSpotifyApi/index.js
+++
@@ -1,4 +0,0 @@
-const { fetchPagedData } = require('./fetchPagedData');
-const { rateLimitedCall } = require('./rateLimitedCall');
-
-module.exports = { fetchPagedData, rateLimitedCall };
